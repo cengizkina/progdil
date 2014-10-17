f4e2bc7b9d5b6b88beba1371997654caae2ed43e
cengizkina <cengiz.kina@bil.omu.edu.tr>
2014-10-17 20:35:23 +0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> </title>
+</head>
+
+<body>
+<form action="delete_underscore.php" method="post">
+Cümlenizi yazınız:<br>
+<input type=text" name="cumle" size = 30px rows = 10px /><br>
+<input type="submit" name="gonder" value="Gönder" />
+</form>
+</body>
+
+</html>
