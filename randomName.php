60514c896f2a4af46961e6a1197496c19eec8227
burakkck <buraakkucuk@gmail.com>
2014-10-21 21:53:25 +0300

    kod tamamlandı

--- randomName.php
+++ randomName.php
@@ -1,46 +1,59 @@
 <?php
 echo randomName("en",5);
 
-	
-	
-function randomName($language, $adet)
-{
+class Random{
 
-	$isim = ["kedi","insan","deniz","masa","dolap","bilgisayar",
-	"kapi","pencere","hafta","gece","gunduz","marti","kazak",
-	"sarisin","sahil","merdiven","ahmet","yol","araba","agac",
-	"yaprak","dal","adam","konuk","ahmet","koca","kadin"];
-    
-	$sifat=["iyi","guzel","hizli","yavas","yakisikli","mert",
-	"cimri","kel","masmavi","ucan","kac","yarim","bu","su"];
-
-	$names = ["baby","child","man","main","house","sea","car",
-	"computer","pencil","pen","doctor","window","line"];
+	public $gecmis = array("ornek kelime");	
+		
+	function randomName($language, $adet)
+	{
 	
-    $adjectives = ["good","beatiful","long","small","nice",
-	"micke","bad","slow","green","blue","hard"];
-
-	$sozluk = array(
-	"tr" => array($isim, $sifat),
-	"en" => array($names, $adjectives)
-	);
+		$isim = array("kedi","insan","deniz","masa","dolap","bilgisayar",
+		"kapi","pencere","hafta","gece","gunduz","marti","kazak",
+		"sarisin","sahil","merdiven","ahmet","yol","araba","agac",
+		"yaprak","dal","adam","konuk","ahmet","koca","kadin");
+	    
+		$sifat= array("iyi","guzel","hizli","yavas","yakisikli","mert",
+		"cimri","kel","masmavi","ucan","kac","yarim","bu","su");
 	
-	$gecmis = [];
-	$i = 0;
-	$isim_boyut = count($sozluk[$language][0]);
-	$sifat_boyut = count($sozluk[$language][1]);
-
+		$names = array("baby","child","man","main","house","sea","car",
+		"computer","pencil","pen","doctor","window","line");
+		
+	    	$adjectives = array("good","beatiful","long","small","nice",
+		"micke","bad","slow","green","blue","hard");
 	
+		$sozluk = array(
+		"tr" => array($isim, $sifat),
+		"en" => array($names, $adjectives)
+		);
+		
+	
+		
+		$isim_boyut = count($sozluk[$language][0]);
+		$sifat_boyut = count($sozluk[$language][1]);
+	
+		$i = 1;
 		while($i <= $adet)
-		{
+			{
 			$index_isim = rand(0,$isim_boyut-1);
 			$index_sifat = rand(0,$sifat_boyut-1);
-			echo $sozluk[$language][1][$index_sifat]." ";
-			echo $sozluk[$language][0][$index_isim] . "</br>";
-			$i++;
+			$kelime = $sozluk[$language][1][$index_sifat]. " " . $sozluk[$language][0][$index_isim] . "</br>"."\n";
+			if ($this->kelimeKontrol($kelime)) {
+				array_push($this->gecmis,$kelime);
+				$i++;
+				echo $kelime;
+			}
 		}
-
 	
-}
+	}
+	
+	function kelimeKontrol($kelime) {
+		if (in_array($kelime, $this->gecmis)) {
+			return false;
+		}else{
+			return true;
+		}
+	}
 
+}
 ?>
