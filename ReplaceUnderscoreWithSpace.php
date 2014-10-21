8c567db5cca362b8d55d07de66b711e68e24f72b
burakkck <buraakkucuk@gmail.com>
2014-10-21 21:10:37 +0300

    Update ReplaceUnderscoreWithSpace.php

--- ReplaceUnderscoreWithSpace.php
+++ ReplaceUnderscoreWithSpace.php
@@ -6,18 +6,18 @@ function ReplaceUnderscoreWithSpace($cumle) {
 	$dizi = str_split($cumle);
 	$first = 0;
 	$last = count($dizi)-1;
-	if ($dizi[$first]=="_"){
+	if ($dizi[$first] == "_"){
 		$first++;
 	}
-	if ($dizi[$last]=="_"){
+	if ($dizi[$last] == "_"){
 		$last--;
 	}
 	for( $i = $first; $i < $last; $i++) {
 	
-		if ($dizi[$i]=="_"){
+		if ($dizi[$i] == "_"){
 			$dizi[$i] = " ";
 		}
 	}
-	return  implode("",$dizi);
+	return  implode("", $dizi);
 }
 ?>
