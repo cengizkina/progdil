f589120828aa9490e0d193ca59c19f5d1595546c
burakkck <buraakkucuk@gmail.com>
2014-10-22 00:49:54 +0300

    Update ReplaceUnderscoreWithSpace.php

--- ReplaceUnderscoreWithSpace.php
+++ ReplaceUnderscoreWithSpace.php
@@ -1,23 +1,31 @@
 <?php
 
-echo ReplaceUnderscoreWithSpace($cumle);
+class Underscore{
 
-function ReplaceUnderscoreWithSpace($cumle) {
-	$dizi = str_split($cumle);
-	$first = 0;
-	$last = count($dizi)-1;
-	if ($dizi[$first] == "_"){
-		$first++;
-	}
-	if ($dizi[$last] == "_"){
-		$last--;
-	}
-	for( $i = $first; $i < $last; $i++) {
-	
-		if ($dizi[$i] == "_"){
-			$dizi[$i] = " ";
+	function ReplaceUnderscoreWithSpace($cumle) {
+		
+		$dizi = str_split($cumle);
+		$first = 0;
+		$last = count($dizi)-1;
+		
+		if ($dizi[$first] == "_"){
+			$first++;
 		}
+		elseif ($dizi[$last] == "_"){
+			$last--;
+		}
+		
+		for( $i = $first; $i < $last; $i++) {
+		
+			if ($dizi[$i] == "_"){
+				$dizi[$i] = " ";
+			}
+		}
+		echo implode("", $dizi) . "\n";
 	}
-	return  implode("", $dizi);
 }
+
+$obj = new Underscore();
+$obj-> ReplaceUnderscoreWithSpace($argv[1]);
+
 ?>
