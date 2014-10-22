d256c1cd0220a2c58a0bd488204631feb25bb911
cengizkina <cengiz.kina@bil.omu.edu.tr>
2014-10-22 16:27:40 +0300

    Update ReplaceUnderscoreWithSpace.php

--- ReplaceUnderscoreWithSpace.php
+++ ReplaceUnderscoreWithSpace.php
@@ -1,40 +1,37 @@
 <?php
-
 class Underscore  {
-
 	function ReplaceUnderscoreWithSpace($word) {
 		if ($this-> justunderscore($word))  {
 			echo $word;
 		}
-		else 
-		{
+		else {
 			$array = str_split($word);
 			$first = 0;
 			$last = count($array)-1;
 			
-		while ($array[$first] == "_")  {
-			$first++;
-		}
-		while ($array[$last] == "_")  {
-			$last--;
-		}
-		
-		for( $i = $first; $i < $last; $i++)  {
-		
-			if ($array[$i] == "_")  {
-				$array[$i] = " ";
+			while ($array[$first] == "_")  {
+				$first++;
 			}
-		}
-		echo implode("", $array) . "\n";
+			while ($array[$last] == "_")  {
+				$last--;
+			}
+			
+			for ($i = $first; $i < $last; $i++)  {
+			
+				if ($array[$i] == "_")  {
+					$array[$i] = " ";
+				}
+			}
+			echo implode("", $array) . "\n";
 	}
 	}
 	function justunderscore($word)	{
-		$array2 = str_split($word);
-		$j  =0;
+		$array = str_split($word);
+		$i  =0;
 		$underscore = true;
-		while($j < count($array2) && $underscore)  {
-			if($array2[$j] == "_")	{
-				$j++;
+		while ($i < count($array) && $underscore)  {
+			if ($array2[$i] == "_")	{
+				$i++;
 				$underscore = true;
 			}
 			else  {
