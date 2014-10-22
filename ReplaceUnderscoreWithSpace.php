abfc57dd968a22c9a77a683158f0b0d301d1a67b
cengizkina <cengiz.kina@bil.omu.edu.tr>
2014-10-22 11:32:16 +0300

    Update ReplaceUnderscoreWithSpace.php

--- ReplaceUnderscoreWithSpace.php
+++ ReplaceUnderscoreWithSpace.php
@@ -1,28 +1,49 @@
 <?php
 
-class Underscore{
+class Underscore  {
 
 	function ReplaceUnderscoreWithSpace($cumle) {
-		
-		$dizi = str_split($cumle);
-		$first = 0;
-		$last = count($dizi)-1;
-		
-		if ($dizi[$first] == "_"){
+		if (justunderscore($word))  {
+			echo $word;
+		}
+		else 
+		{
+			$dizi = str_split($cumle);
+			$first = 0;
+			$last = count($dizi)-1;
+			
+		while ($dizi[$first] == "_")  {
 			$first++;
 		}
-		elseif ($dizi[$last] == "_"){
+		while ($dizi[$last] == "_")  {
 			$last--;
 		}
 		
-		for( $i = $first; $i < $last; $i++) {
+		for( $i = $first; $i < $last; $i++)  {
 		
-			if ($dizi[$i] == "_"){
+			if ($dizi[$i] == "_")  {
 				$dizi[$i] = " ";
 			}
 		}
 		echo implode("", $dizi) . "\n";
 	}
+	}
+	function justunderscore($word)	{
+		$array2 = str_split($word);
+		$j=0;
+		$underscore = true;
+		while($j < count($array2)&& $underscore)  {
+			if($array2[$j] == "_")	{
+				$j++;
+				$underscore = true;
+			}
+			else  {
+				$underscore = false;
+			}
+		}
+		return $underscore;
+	} 
+	
 }
 
 $obj = new Underscore();
